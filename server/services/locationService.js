bb54e3238be6d8f52d1830512e61701c9be2ed78
abhijeet113 <shindeabhijeet113@gmail.com>
2025-02-22 12:36:52 +0530

    basic setup

---
+++ server/services/locationService.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+export async function getNearestLocations(longitude, latitude, distance) {
+    // return await LocationRepo.getNearestLocations(longitude, latitude);
+    try {
+        console.log("🔍 Finding nearest locations");
+        const locations = await User.find({
+            location: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [longitude, latitude]
+                    }
+                }
+            }
+        }).limit(5); // You can adjust the limit as needed
+        return locations;
+    } catch (err) {
+        console.log("Error in finding locations", err);
+        return err;
+    }
+}
